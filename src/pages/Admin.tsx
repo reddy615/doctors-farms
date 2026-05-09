5f5d2ade6e5da1cbabe59213554e93c71fd4e496
reddy615 <polareddyakshayreddy@gmail.com>
2026-05-09 23:25:44 +0530

    Label inquiry times as IST

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -42,6 +42,7 @@ export default function Admin() {
         hour: '2-digit',
         minute: '2-digit',
         hour12: true,
+        timeZoneName: 'short',
       }).format(d);
     } catch {
       return val;
